ff76c542694d54c8cb0165701ccc62aa9530037d
Shon Saevang <ssaevang@jgboswell.com>
2021-01-30 21:49:47 -0800

    Rename service implementation

--- DadJokes.Api/DadJokeService.cs
+++ DadJokes.Api/DadJokeService.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DadJokes.Api
 {
-    public class JokeService : IJokeService
+    public class DadJokeService : IJokeService
     {
+        private HttpClient _httpClient;
+
+        public DadJokeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public string GetRandomJoke()
         {
             throw new NotImplementedException();
