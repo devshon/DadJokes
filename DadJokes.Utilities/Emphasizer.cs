9d6eed04367a533d6b5f28ffbafa214ce194cf55
Shon Saevang <ssaevang@jgboswell.com>
2021-01-31 01:49:05 -0800

    Add utilities project

---
+++ DadJokes.Utilities/Emphasizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DadJokes.Utilities
+{
+    public static class Emphasizer
+    {
+        public static string EmphasizeWithUppercase(this string input, string wordToEmphasize)
+        {
+            string output = input
+                .Replace(wordToEmphasize.ToLower(), wordToEmphasize.ToUpper())
+                .Replace(Char.ToUpper(wordToEmphasize[0]) + input.Substring(1), wordToEmphasize.ToUpper());
+
+            return output;
+        }
+    }
+}
