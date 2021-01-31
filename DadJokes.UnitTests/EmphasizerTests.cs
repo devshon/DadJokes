28ac961df90c14894653b07961ba9b117e615e0a
Shon Saevang <ssaevang@jgboswell.com>
2021-01-31 01:55:17 -0800

    Add unit test project

---
+++ DadJokes.UnitTests/EmphasizerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DadJokes.UnitTests
+{
+    [TestClass]
+    public class EmphasizerTests
+    {
+        [TestMethod]
+        public void EmphasizeWithUpper_AllLowerCase_ReturnsCorrectOutput()
+        {
+        }
+    }
+}
