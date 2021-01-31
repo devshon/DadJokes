b0160c3f2f6021953fa3454c22ec887cba458935
Shon Saevang <ssaevang@jgboswell.com>
2021-01-31 02:03:03 -0800

    Add grouper class

---
+++ DadJokes.Utilities/Grouper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadJokes.Utilities
+{
+    public static class Grouper
+    {
+        public static IEnumerable<IEnumerable<string>> GroupByWordLength(IEnumerable<string> strings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
