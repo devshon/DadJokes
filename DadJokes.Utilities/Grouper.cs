dc42e63a49b2ad40475e00deabbce4876450df78
Shon Saevang <ssaevang@jgboswell.com>
2021-01-31 02:04:47 -0800

    Add comment to grouper

--- DadJokes.Utilities/Grouper.cs
+++ DadJokes.Utilities/Grouper.cs
@@ -10,6 +10,7 @@ namespace DadJokes.Utilities
     {
         public static IEnumerable<IEnumerable<string>> GroupByWordLength(IEnumerable<string> strings)
         {
+            // three groups: <10 words, <20 words, and >= 20 words.
             throw new NotImplementedException();
         }
     }
