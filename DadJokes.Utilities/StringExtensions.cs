ac8cd99078785f05c164d82150e72158ddeb0733
Shon Saevang <ssaevang@jgboswell.com>
2021-02-01 14:06:06 -0800

    Fix emphasize method

--- DadJokes.Utilities/StringExtensions.cs
+++ DadJokes.Utilities/StringExtensions.cs
@@ -24,7 +24,7 @@ namespace DadJokes.Utilities
 
             string output = input
                 .Replace(termToEmphasize.ToLower(), termToEmphasize.ToUpper())
-                .Replace(Char.ToUpper(termToEmphasize[0]) + input.Substring(1), termToEmphasize.ToUpper());
+                .Replace(Char.ToUpper(termToEmphasize[0]) + termToEmphasize.Substring(1), termToEmphasize.ToUpper());
 
             return output;
         }
