de5f3c1ef8f76a255ab7e29ff9178c0406edc454
Shon Saevang <ssaevang@jgboswell.com>
2021-01-31 16:04:12 -0800

    Implement grouper

--- DadJokes.Utilities/Grouper.cs
+++ DadJokes.Utilities/Grouper.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DadJokes.Utilities
 {
     public static class Grouper
     {
-        public static IEnumerable<IEnumerable<string>> GroupByWordLength(IEnumerable<string> strings)
+        public static IList<IEnumerable<string>> GroupByWordLength(IEnumerable<string> strings)
         {
             // three groups: <10 words, <20 words, and >= 20 words.
-            throw new NotImplementedException();
+            var lessThan10 = strings.Where(x => x.NumberOfWords() < 10);
+            var lessThan20GreaterThanOrEqualTo10 = strings.Where(x => x.NumberOfWords() < 20).Where(x => x.NumberOfWords() >= 10);
+            var greaterThanOrEqualTo20 = strings.Where(x => x.NumberOfWords() >= 20);
+
+            var groups = new List<IEnumerable<string>>();
+            groups.Add(lessThan10);
+            groups.Add(lessThan20GreaterThanOrEqualTo10);
+            groups.Add(greaterThanOrEqualTo20);
+
+            return groups;
+        }
+
+        // TODO: Need to move or rename class
+        public static int NumberOfWords(this string input)
+        {
+            return input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
         }
     }
 }
