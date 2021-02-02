3a81e5800670ed1cdc5b202517e570846e51044b
Shon Saevang <ssaevang@jgboswell.com>
2021-02-02 00:02:54 -0800

    Add JokeSize constants class

---
+++ DadJokes.Api/Entities/JokeSize.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DadJokes.Api.Entities
+{
+    public static class JokeSize
+    {
+        public const int Short = 0;
+        public const int Medium = 10;
+        public const int Long = 20;
+    }
+}
