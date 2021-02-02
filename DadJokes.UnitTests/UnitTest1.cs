729a5ab2aa1592d00697159b4314548ff9437d64
Shon Saevang <ssaevang@jgboswell.com>
2021-02-01 20:19:58 -0800

    Add unit tests project

---
+++ DadJokes.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DadJokes.UnitTests
+{
+    [TestClass]
+    public class ClassNameTests
+    {
+        [TestMethod]
+        public void MethodName_Conditions_ExpectedOutcome()
+        {
+        }
+    }
+}
